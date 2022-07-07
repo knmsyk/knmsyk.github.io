505dc046b5cda0fb71cb8415bcd557f8baa3af2a
msyk <kouno@msyk.works>
2022-07-07 20:11:21 +0900

    Add works page

--- lib/products.rb
+++ lib/products.rb
@@ -2,7 +2,7 @@ Product = Struct.new :name, :cover, :platform, :release, :text, :quote
 
 def products
   [
-    Product.new('eny', 'eny.png', 'iOS', 'Coming in Spring 2022', 'parts/eny-text', 'parts/eny-quote'),
+    Product.new('eny', 'eny.png', 'iOS', 'Coming late 2022', 'parts/eny-text', 'parts/eny-quote'),
     Product.new('Dots', 'dots.png', 'iOS', 'Coming Soon', 'parts/dots-text', 'parts/dots-quote'),
   ]
 end

83197b6d3e8f4bec396a1dc1dacc4741aa61e13c
msyk <kounomasayuki@gmail.com>
2022-12-31 09:35:56 +0900

    Update Dots

--- lib/products.rb
+++ lib/products.rb
@@ -1,8 +1,27 @@
-Product = Struct.new :name, :cover, :platform, :release, :text, :quote
+Link = Struct.new :title, :url
+Product = Struct.new :name, :cover, :platform, :release, :text, :quote, :links
 
-def products
+def products 
   [
-    Product.new('eny', 'eny.png', 'iOS', 'Coming late 2022', 'parts/eny-text', 'parts/eny-quote'),
-    Product.new('Dots', 'dots.png', 'iOS', 'Coming Soon', 'parts/dots-text', 'parts/dots-quote'),
+    Product.new(
+      'eny',
+      'eny.png',
+      'iOS',
+      'Coming soon',
+      'parts/eny-text',
+      'parts/eny-quote',
+      []
+    ),
+    Product.new(
+      'Dots',
+      'dots.png',
+      'iOS',
+      'Released in 2022',
+      'parts/dots-text',
+      'parts/dots-quote',
+      [
+        Link.new('AppStore', 'https://apps.apple.com/app/id6443408878')
+      ]
+    )
   ]
 end
